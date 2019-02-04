1f1879df1aaae3e16d0eaf55921ea7ba8fad7cde
Levon K. Mkrtchyan <levon.mkrtchyan@gmail.com>
2019-02-04 15:13:42 -0500

    improve invalid uuid message

--- find_collisions.js
+++ find_collisions.js
@@ -7,7 +7,7 @@ process.stdin
   .pipe(split(undefined, undefined, { trailing: false }))
   .pipe(reduce((acc, data) => {
     if(!/^\w{8}-\w{4}-4\w{3}-\w{4}-\w{12}$/.test(data)){
-      console.log(data)
+      console.log(`Invalid UUIDv4: <${data}>`);
     }
     acc[data] = (acc[data] || 0) + 1;
     return acc;
